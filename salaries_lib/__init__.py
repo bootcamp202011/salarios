8adf9d01e651e87d95f24cf751958228a059d3ee
Daniela <dcardonaa2111@gmail.com>
2021-03-13 23:31:15 -0700

    [Feat] Adición de funciones.

--- salaries_lib/__init__.py
+++ salaries_lib/__init__.py
@@ -1,4 +1,17 @@
+<<<<<<< HEAD
 from .fsalaries import mayus 
 from .fsalaries import download_data
 from .fsalaries import sel
 from .fsalaries import divide
+=======
+from .fsalaries import download_data
+from .fsalaries import sel
+from .fsalaries import mayus 
+from .fsalaries import rename_columns
+from .fsalaries import divide
+from .fsalaries import box_plot
+from .fsalaries import outliers_percent
+from .fsalaries import remove_outliers
+from .fsalaries import standarization
+from .fsalaries import dist_hist
+>>>>>>> 67b6144 ([Init][Feat] Inicialización de repositorio y adición de archivos de la librería Tech_Salaries.)
